dc9869388a4291a757694a409dd47cd3979d1350
Oleg-Sachuk <Sachuk.o.a@gmail.com>
2021-04-16 23:38:56 +0300

    Added dist plain subfolder

---
+++ client/src/hook/RequestHttp.js
@@ -0,0 +1,38 @@
+import { useCallback, useState } from "react"
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+        setLoading(true);
+        try {
+
+            if (body) {
+                body = JSON.stringify(body);
+                headers['Content-Type'] = 'application/json'
+            }
+            
+            debugger;
+            const response = await fetch(url, { method, body, headers })
+            const data = response.json();
+            await data.then(res => { return setError(res.message) });
+
+            if (!response.ok) {
+                setLoading(false);
+                return new Error(error || "Something went wrong");
+            }
+
+            setLoading(false);
+            return data;
+        } catch (error) {
+            setLoading(false);
+            setError(error.message);
+            throw error;
+        }
+    }, [error])
+
+    const clearError = useCallback(() => setError(null), []);
+
+    return { loading, request, error, clearError }
+}
